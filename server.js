0f9106fbdd8faedad01207eec436f2baa98ca386
Dearie <eburujacintha@gmail.com>
2024-07-02 12:27:25 +0100

    solved ip address issue

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+import express from 'express';
+import { publicIp } from 'public-ip';
 
-const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  const ipAddress = req.ip;
-  res.send(`Your IP address is: ${ipAddress}`);
+app.get('/', async (req, res) => {
+  try {
+    const ipAddress = await publicIp();
+    res.send(`Your public IP address is: ${ipAddress}`);
+  } catch (error) {
+    res.status(500).send('Error getting public IP address');
+  }
 });
 
 app.listen(3000, () => {
