9fd2e76d216ce8b0534949d658d77a5eda4a4347
Dearie <eburujacintha@gmail.com>
2024-07-02 11:38:57 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  const ip = 
+  request.headers['x-real-ip'] ||
+  request.headers['x-forwarded-for'] ||
+  
+  request.socket.remoteAddress || '';
+
+  return response.json({
+    ip,
+  })
+})
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
